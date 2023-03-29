2bb3a923297d3e0d47d37589f694a9b732d089f5
boydlakers15 <jboyd115@icloud.com>
2023-03-29 09:48:08 +0200

    Made changes tp Navbar.js and added some style

--- cookbook/src/comp/Navbar.js
+++ cookbook/src/comp/Navbar.js
@@ -1,11 +1,58 @@
+import React, { useState } from 'react';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [show, setShow] = useState(false);
     return (
 
     <nav className="nav">
       <p className="section"><img className='img' src='https://img.freepik.com/free-vector/restaurant-logo_23-2147506959.jpg?w=360'></img><br/>Recipes</p>
       
+      <main className={show ? 'space-toggle' : null}>
+      <header className={`header ${show ? 'space-toggle' : null}`}>
+        <div className='header-toggle' onClick={() => setShow(!show)}>
+          <i className={`fas fa-bars ${show ? 'fa-solid fa-xmark' : null}`}></i>
+        </div>
+      </header>
 
+      <aside className={`sidebar ${show ? 'show' : null}`}>
+        <nav className='nav'>
+          <div>
+            <Link to='/' className='nav-logo'>
+              <i className={`fas fa-home-alt nav-logo-icon`}></i>
+              <span className='nav-logo-name'>Homepage</span>
+            </Link>
+
+            <div className='nav-list'>
+              <Link to='/dashboard' className='nav-link active'>
+                <i className='fas fa-tachometer-alt nav-link-icon'></i>
+                <span className='nav-link-name'>Dashboard</span>
+              </Link>
+              <Link to='/hotel' className='nav-link'>
+                <i className='fas fa-hotel nav-link-icon'></i>
+                <span className='nav-link-name'>Hotel</span>
+              </Link>
+              <Link to='/gallery' className='nav-link'>
+                <i className='fas fa-image nav-link-icon'></i>
+                <span className='nav-link-name'>Gallery</span>
+              </Link>
+              <Link to='/gallery' className='nav-link'>
+                <i className='fas fa-dollar-sign nav-link-icon'></i>
+                <span className='nav-link-name'>Transaction</span>
+              </Link>
+            </div>
+          </div>
+
+          <Link to='/logout' className='nav-link'>
+            <i className='fas fa-sign-out nav-link-icon'></i>
+            <span className='nav-link-name'>Logout</span>
+          </Link>
+        </nav>
+      </aside>
+
+      
+    </main>
     </nav>
     )
     
